538cc24a9c6d8f0347f2d86e27efbc92e475face
Poilou <labiloute@neuf.fr>
2014-09-13 16:03:45 +0100

    Ajout de ma biere irlandaise pref

--- assets/js/BeerList.js
+++ assets/js/BeerList.js
@@ -76,4 +76,8 @@ BeerImage = new Array();
     BeerImage["1664"] = "beer3";
     BeerName["1664"] = "1664";
     BeerList["1664"] = draw_beer("https://overpass-api.de/api/interpreter?data=[out:json];node(BBOX)['brewery'~'1664'];out;", "assets/img/"+BeerImage['1664']+".png");
+    // Coucou !
+    BeerImage["rebelred"] = "beer4";
+    BeerName["rebelred"] = "Rebel Red";
+    BeerList["rebelred"] = draw_beer("https://overpass-api.de/api/interpreter?data=[out:json];node(BBOX)['brewery'~'[rR]ebel red'];out;", "assets/img/"+BeerImage['rebelred']+".png");
 //les fonctions utilisées pour récupérer les données et les placer sont dans BeerLayer.js
