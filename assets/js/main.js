35dd41b2a25371d49c861e081186f4bcccbe7c73
Noémie Lehuby <noemie.lehuby@centrale-marseille.fr>
2014-10-18 15:43:56 +0000

    ajout de n'importe quelle bière dans OSM, en passant par le webservice de Poilou pour récupérer le nom

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,3 +114,32 @@ var hash = new L.Hash(map);
 
 /* supprimer la barre de progression quand tout le js est traité */
 $(document).one("ajaxStop", function () {$("#loading").hide(); });
+
+/* add any beer to the edit bar form, thanks to Poilou webservice */
+   $( "#beer-other" ).autocomplete({
+         source: function( request, response) {
+         $.ajax({
+            url: "http://92.243.10.86/json.php?",
+            dataType: "jsonp",
+            data: {
+            q: request.term
+            },
+            success: function( data ) {
+            //console.log(data);
+               ListData = [];
+               for (var i in data) {
+                  if (data[i].Beername && typeof data[i].Beername != 'undefined') {
+                     ListData.push(data[i].BeerTag);
+                     }
+                  }
+               response(ListData);
+               }
+            });
+         },
+         minLength: 3
+   });
+
+        $("#addbutton").on('click', function () {
+        $('#pubeditbeerlist').append('<div class="checkbox"><label><input type="checkbox" name="beer" checked="true" value="' + $("#beer-other").val() +'">'+$("#beer-other").val()+'</label></div>');
+        $('#beer-other').val("");
+    });
