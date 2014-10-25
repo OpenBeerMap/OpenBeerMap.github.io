8a208f2622fdcff4d879c6ad89a508c46aff31d9
Noémie Lehuby <noemie.lehuby@centrale-marseille.fr>
2014-10-25 17:14:31 +0000

    retour à des checkbox dans la modale de personalisation

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -4,11 +4,8 @@
 
 //les variables liées aux données sur les bières sont dans BeerList.js
 
-
 function init_localstorage() { 
-      localstorageitems = localStorage.length // Count items in local storage
-
-	// set default for the first visit
+  	// set default for the first visit
     if (localStorage.length == 0) {
             localStorage.setItem('chouffe','beers/chouffe.png'); 
             localStorage.setItem('affligem','beers/affligem.png'); 
@@ -26,9 +23,7 @@ function init_localstorage() {
    		 	BeerList[element] = draw_beer("https://overpass-api.de/api/interpreter?data=[out:json];(node(BBOX)[\"brewery\"~\""+element+"\",i];way(BBOX)[\"brewery\"~\""+element+"\",i]);out center;", "assets/img/"+BeerImage[element]); 
       }  
    
-       LocalStorageList();
-	
-      //deprecated-UpdateBeerList_Setup_Form(); // Update beers in setup form
+      LocalStorageList();
       
       UpdateBeerList_Edition_Form(); // update beers list in OSM form
      
@@ -41,42 +36,6 @@ function RefreshTxt() {
      document.getElementById('ResultLocalStorage').innerHTML = localStorage.length;
   };
   
-  
-/* deprecated func*/
-function Store(element) {
-     if (element.checked) {
-      // Add item to localStorage
-      localStorage.setItem(element.value,'1');
-   }
-   else { 
-      // Remove item from localStorage
-      if (map.hasLayer(BeerList[element.value])) {map.removeLayer(BeerList[element.value]);}
-      delete localStorage.removeItem(element.value);
-
-   }  
-   RefreshTxt(); // Refresh notification
-  };
-  
-/* deprecated func*/  
-function UpdateBeerList_Setup_Form() {
-      var htmlBieres = '<input type="text" id="beersearchinput" name="beersearchinput" onChange="BeerSearch(this.value);"/><br />';
-
-      var checked = '';
-      for (var myi in BeerName) {
-         if (BeerName.hasOwnProperty(myi)) {
-            var TempBeerName = BeerName[myi];
-            var TempBeerNameLowercase = myi;
-            if (localStorage.getItem(TempBeerNameLowercase) != null) {checked = 'checked="checked"';}
-            else {checked = '';}
-            TempLine = '<input type="checkbox" '+checked+' name="'+ TempBeerNameLowercase +'" value="' + TempBeerNameLowercase + '" id="' + TempBeerNameLowercase + '" onClick="Store(this)" />&nbsp;&nbsp;' + TempBeerName + '<br />';
-            htmlBieres += TempLine;
-            }
-         }
-      
-      document.getElementById('FormSelectedBeers').innerHTML = htmlBieres;
-  };
-/* end deprecated */ 
-
 function UpdateBeerList_Edition_Form() {
      var htmlBeers = '          <label class="col-md-4 control-label" for="checkboxes" data-l10n-id="liste_des_bieres">Bière pression dispo</label><div id="checkboxlist" class="col-md-4">';
      i=0;
@@ -91,9 +50,6 @@ function UpdateBeerList_Edition_Form() {
       }
      document.getElementById('pubeditbeerlist').innerHTML = htmlBeers;
 };
-
-
- 
 
 function LocalStorageStore(element,value) {
       if (localStorage.getItem(element) == null) {
@@ -113,9 +69,10 @@ function LocalStorageStore(element,value) {
 function LocalStorageList() {
       LSlength = localStorage.length;
       TxtList='';
-      for (i=0; i<LSlength; i++) {
-         //console.log(i + " : " + localStorage.key(i));
-         TxtList += '<li><input type="button" class="removebutton" value="'+ localStorage.key(i) +'" /></li>';
+      for (i=0; i<LSlength; i++) {    
+		  console.log(localStorage.key(i))
+		  console.log(localStorage[i])
+		TxtList += '<label><input type="checkbox"  checked name="'+ localStorage.key(i) +'" value="' + localStorage.key(i) + '" id="' + localStorage.key(i) + '" onClick="LocalStorageStore(this.value,localStorage[this.value])" />&nbsp;&nbsp;' + localStorage.key(i) + '</label><br />'		  
       }
       $( "#localstoragelist" ).html(TxtList);
    }
